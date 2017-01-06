3b48a4a4e7bd448f635814d606bfea94b1854f5f
Geert Vanderpijpen <geert.vanderpijpen@gmail.com>
2017-01-06 09:40:17 +0100

    Nested interfaces
    Shows how nested interfaces behave and how they can or cannot be
    accessed by implementation classes.

---
+++ ThinkingInJava/src/be/geertvanderpijpen/thinkinginjava/examples/interfaces/Months.java
@@ -0,0 +1,10 @@
+package be.geertvanderpijpen.thinkinginjava.examples.interfaces;
+
+// Using an interface to create groups of constants. 
+// Enumerators should be used for this functionality
+
+public interface Months {
+	int JANUARI = 1, FEBRUARY = 2, MARCH = 3, APRIL = 4, MAY = 5, JUNE = 6,
+			JULY = 7, AUGUST = 8, SEPTEMBER = 9, OCTOBER = 10, NOVEMBER = 11,
+			DECEMBER = 12;
+}
